09f6bf1db93f8e9c8d997604d42af1970edd0873
shubham kedia <subham.kedia@oriserve.com>
2023-10-08 03:59:19 +0530

    task 3

--- src/components/Task3/index.js
+++ src/components/Task3/index.js
@@ -1,10 +1,28 @@
+import { useState } from "react"
 import { Box } from "@mui/material"
 import { green } from "@mui/material/colors"
 
 const Task3 = () => {
+  const [bgColor, setBgColor] = useState(green[50])
+
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event
+
+    const yInterval = Math.floor((window.innerHeight - 64) / 28)
+    const xInterval = Math.floor(window.innerWidth / 28)
+
+    const xposition = Math.floor(clientX / xInterval) % 14
+    const yPosition = Math.floor((clientY - 64) / yInterval) % 14
+
+    if (xposition > yPosition) {
+      setBgColor(green[Object.keys(green)[xposition]])
+    } else {
+      setBgColor(green[Object.keys(green)[yPosition]])
+    }
+  }
   return (
     <Box
-      bgcolor={green[50]}
+      bgcolor={bgColor}
       position="fixed"
       top={64}
       left={0}
@@ -13,6 +31,7 @@ const Task3 = () => {
       display="flex"
       alignItems="center"
       justifyContent="center"
+      onMouseMove={handleMouseMove}
     >
       Move Cursor to See Effects
     </Box>
