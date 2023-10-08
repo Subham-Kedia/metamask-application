c940f742bfce1f583e95342c25b2d1e53caacffa
shubham kedia <subham.kedia@oriserve.com>
2023-10-08 16:32:13 +0530

    changes'

--- src/data/config/task5.js
+++ src/data/config/task5.js
@@ -24,6 +24,7 @@ function sumPairs(input, target) {
 
 // test cases
 
+// empty case
 console.log(sumPairs([], 2))
 console.log(
   sumPairs([23, 2323, 4242, 423, 12, 23, 444, 5, 445, 654, 35, 35, 353, 5], 425)
@@ -31,3 +32,4 @@ console.log(
 // same number multiples times
 console.log(sumPairs([1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1], 3))
 console.log(sumPairs([2, 3, 2, 2], 4))
+console.log(sumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 20))
