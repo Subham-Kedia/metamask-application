1bb2862272faf6211783cbddca918ab562834047
shubham kedia <subham.kedia@oriserve.com>
2023-10-08 12:43:04 +0530

    feat(task 5): completed

---
+++ src/data/config/task5.js
@@ -0,0 +1,33 @@
+// Task 5
+
+function sumPairs(input, target) {
+  if (input.length < 2) return []
+
+  const result = []
+
+  for (let i = 0; i < input.length - 1; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] + input[j] === target) {
+        // addition check if(same numbers appearing multiple times)
+        if (
+          result.findIndex((item) => {
+            return item.includes(input[i]) && item.includes(input[j])
+          }) === -1
+        )
+          result.push([input[i], input[j]])
+      }
+    }
+  }
+
+  return result
+}
+
+// test cases
+
+console.log(sumPairs([], 2))
+console.log(
+  sumPairs([23, 2323, 4242, 423, 12, 23, 444, 5, 445, 654, 35, 35, 353, 5], 425)
+)
+// same number multiples times
+console.log(sumPairs([1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1], 3))
+console.log(sumPairs([2, 3, 2, 2], 4))
